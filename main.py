7b3dd10cf25eca6342635ed33bc4e29649c26ad8
089487 <46375526+089487@users.noreply.github.com>
2020-08-31 21:08:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import sys
 import requests
 import re
+import sys
+import os
+function={"http://www.cpbl.com.tw/":{"news":'^/news/view/'}}
 class Crawler():
     def __init__(self):
         self.session=requests.session()
@@ -11,19 +13,35 @@ class Crawler():
                 'Accept': 'text/html,application/xhtml+xml,application/xml;'
                 'q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
                 }
-    
+    def setforchinise(self,web):
+        type=sys.getfilesystemencoding()
+        web.encode(type)
+        return web
     def findhref(self,tag,limit):
         return self.bs.find_all(tag,{'href':re.compile(limit)})
-    def parse(self,url):
+    def news(self,url):
+        #os.system("cls")
+        #print(url)
+        html=urlopen(url)
+        page=self.setforchinise(html.read().decode('utf-8'))
+        #print(page)
+        
+        bs=BeautifulSoup(page,'html.parser')
+        l=bs.find_all('p')
+        for text in l:
+            print(text.string)
+    def parse(self,url,chose):
         print(url)
         html=urlopen(url)
-        page=html.read()
+        page=self.setforchinise(html.read().decode('utf-8'))
         print(page)
+        limit=function[url][chose]
         self.bs=BeautifulSoup(page,'html.parser')
-        div=self.findhref('a','^/news/view/')
-        print("div",div)
-        for tag in div:
-            print(tag.string,':',str(url+tag['href']))
+        l=self.findhref('a',limit)
+        if chose=="news":
+            for tag in l:
+                self.news(str(url+tag['href']))
+                print(tag.string,':',)
 crawler=Crawler()
-crawler.parse("http://www.cpbl.com.tw/")
+crawler.parse("http://www.cpbl.com.tw/","news")
 #print(bs.prettify())
